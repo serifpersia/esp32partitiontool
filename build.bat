a41e43140629bcbb084fcc581e86b644c069bfe2
serifpersia <ramiserifpersia@gmail.com>
2024-02-19 20:38:13 +0100

    fully working .sh and .bat build scripts

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+set "script_dir=%~dp0"
+set "source_folder=%script_dir%src"
+set "dependencies_folder=%script_dir%dependencies"
+set "bin_folder=%script_dir%bin"
+set "tool_folder=%script_dir%tool"
+
+mkdir "%bin_folder%" 2>nul
+cd /d "%source_folder%" || exit /b
+javac -cp "%dependencies_folder%\pde.jar;%dependencies_folder%\arduino-core.jar;%dependencies_folder%\commons-codec-1.7.jar" -d "%bin_folder%" *.java
+mkdir "%tool_folder%" 2>nul
+mkdir "%tool_folder%\ESP32PartitionTool\tool" 2>nul
+jar cvf "%tool_folder%\ESP32PartitionTool\tool\ESP32PartitionTool.jar" -C "%bin_folder%" .
+rmdir /s /q "%bin_folder%"
+
+echo JAR file created successfully.
+pause
+endlocal
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the directory of the script
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+# Define folder names
+source_folder="$script_dir/src"
+dependencies_folder="$script_dir/dependencies"
+bin_folder="$script_dir/bin"
+tool_folder="$script_dir/tool"
+
+# Create necessary folders
+mkdir -p "$bin_folder" "$tool_folder" "$tool_folder/ESP32PartitionTool/tool"
+
+# Compile Java files
+javac -cp "$dependencies_folder/pde.jar:$dependencies_folder/arduino-core.jar:$dependencies_folder/commons-codec-1.7.jar" -d "$bin_folder" "$source_folder"/*.java
+
+# Package class files into a JAR file
+jar cvf "$tool_folder/ESP32PartitionTool/tool/ESP32PartitionTool.jar" -C "$bin_folder" .
+
+# Remove bin folder
+rm -rf "$bin_folder"
+
+echo "JAR file created successfully."
