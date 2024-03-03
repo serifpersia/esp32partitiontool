796699fe3929f4a002ac81542923347f351e03a8
tobozo <tobozo@users.noreply.github.com>
2024-03-03 10:28:58 +0100

    added resources folder

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 
 # Define folder names
 source_folder="$script_dir/src/main/java/com/serifpersia/esp32partitiontool"
-dependencies_folder="$script_dir/dependencies"
+deps_dir="$script_dir/dependencies"
 bin_folder="$script_dir/bin"
 tool_folder="$script_dir/tool"
 
@@ -13,10 +13,19 @@ tool_folder="$script_dir/tool"
 mkdir -p "$bin_folder" "$tool_folder" "$tool_folder/ESP32PartitionTool/tool"
 
 # Compile Java files
-javac -cp "$dependencies_folder/pde.jar:$dependencies_folder/arduino-core.jar:$dependencies_folder/commons-codec-1.7.jar" -d "$bin_folder" "$source_folder"/*.java
+if javac -cp "$deps_dir/pde.jar:$deps_dir/arduino-core.jar:$deps_dir/commons-codec-1.7.jar:$script_dir/resources/*" -d "$bin_folder" "$source_folder"/*.java -Xlint:deprecation; then
+  echo "Files compiled successfully"
+else
+  exit 1
+fi
+
 
 # Package class files into a JAR file
-jar cvf "$tool_folder/ESP32PartitionTool/tool/ESP32PartitionTool.jar" -C "$bin_folder" .
+if jar cvf "$tool_folder/ESP32PartitionTool/tool/ESP32PartitionTool.jar" -C "$bin_folder" . ; then
+  echo "Package created successfully"
+else
+  exit 1
+fi
 
 # Remove bin folder
 rm -rf "$bin_folder"
